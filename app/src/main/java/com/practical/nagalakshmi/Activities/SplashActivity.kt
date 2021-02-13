514864d03864e97e0482d5806e50df181e0142ec
nagalakshmi-palyam <nagalakshmi.palyam@gmail.com>
2021-02-13 14:16:00 +0530

    #  Fusion Practical Assignment

---
+++ app/src/main/java/com/practical/nagalakshmi/Activities/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.practical.nagalakshmi.Activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.practical.nagalakshmi.R
+
+class SplashActivity : AppCompatActivity() {
+    private val SPLASH_DISPLAY_LENGTH = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+    override fun onResume() {
+        super.onResume()
+        Handler().postDelayed({
+            val mainIntent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, SPLASH_DISPLAY_LENGTH.toLong())
+    }
+}
